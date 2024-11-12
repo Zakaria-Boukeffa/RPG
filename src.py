1f6de2ddd2da71d8ea5e784accb5a10d21a6576c
Zakaria Boukeffa <zakaria.boukeffa.03@gmail.com>
2024-11-12 21:22:07 +0100

    [Green] Mort a 10

--- src.py
+++ src.py
@@ -4,6 +4,13 @@ class Personnage:
 
     def get_hp(self):
         return self.hp  
+    
     def recevoir_attaque(self, attaquant):  
         if self.hp > 0 :
             self.hp -= 1
+            
+    def est_mort(self):  
+        if self.hp == 0 :
+            return 1
+        else :
+            return 0
