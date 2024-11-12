25b861f2a3e496f537599bb2e9e4d624b44f4a4f
Zakaria Boukeffa <zakaria.boukeffa.03@gmail.com>
2024-11-12 20:57:28 +0100

    [RED] Recevoir attaque

--- rpg_test.py
+++ rpg_test.py
@@ -1,9 +1,16 @@
 import unittest
 from src import Personnage
 class TestPersonnage(unittest.TestCase):
+    
     def test_perso_10_creation(self):
         personnage = Personnage()
         self.assertEqual(personnage.get_hp(), 10)
+        
+    def test_attaque(self):
+         Perso_att = Personnage()
+         Perso_defen = Personnage() 
+         Perso_defen.recevoire_attaque()
+         self.assertEqual(Perso_defen.get_hp(), 9)
 
 
 if __name__ == '__main__':
