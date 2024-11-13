90be3d06286c848b02fbd914df9358c176724de6
Zakaria Boukeffa <zakaria.boukeffa.03@gmail.com>
2024-11-13 17:43:45 +0100

    [GREEN]se_soigner

--- src.py
+++ src.py
@@ -1,16 +1,17 @@
 class Personnage:
     def __init__(self):
-        self.hp = 10  
+        self.hp = 10
 
     def get_hp(self):
-        return self.hp  
-    
-    def recevoir_attaque(self, attaquant):  
-        if self.hp > 0 :
+        return self.hp
+
+    def recevoir_attaque(self, attaquant):
+        if self.hp > 0:
             self.hp -= 1
-            
-    def est_mort(self):  
-        if self.hp == 0 :
-            return 1
-        else :
-            return 0
+
+    def est_mort(self):
+        return self.hp == 0
+
+    def se_soigner(self):
+        if self.hp < 10:
+            self.hp += 1
